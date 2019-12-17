1ec9631d4a23bc16d8a0f746e91964b3a2babb97
natsu-summer72 <yjikxipad@gmail.com>
2019-12-17 22:08:47 +0900

    combine question with answer

---
+++ Assets/OpenAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenAnswer : MonoBehaviour {
+    public Sprite image;
+
+    public void openAns(){
+            Sprite image = Resources.Load<Sprite> ("answer/ans"+ ChangeImage.getId().ToString());
+            this.gameObject.GetComponent<Image> ().sprite = image;
+    }
+}
